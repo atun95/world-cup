ca1f4caa3fa8c69fff05ec9d3fa8303a40c2730b
atun95 <98630087+atun95@users.noreply.github.com>
2026-06-18 22:02:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import streamlit as streamlit.components.v1 as components
+import streamlit as st
+import streamlit.components.v1 as components
 
-# Đọc nội dung file index.html
+st.title("Ứng dụng World Cup của tôi")
+
 with open("index.html", "r", encoding="utf-8") as f:
     html_code = f.read()
 
-# Hiển thị file HTML lên Streamlit (thay đổi chiều rộng/cao tùy ý)
 components.html(html_code, height=600, scrolling=True)
